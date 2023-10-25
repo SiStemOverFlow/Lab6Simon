29cb6b7b3332dead4682b7b5760bba4231a7864e
SiStemOverFlow <scorrearigonato@ufl.edu>
2023-10-25 12:27:10 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def menu():
+    print("Menu\n" + "-------------\n" + "1. Encode\n" + "2. Decode\n" + "3. Quit\n")
+    choice = int(input("Please enter an option: "))
+    password = int(input("Please enter your password to encode: "))
+    print("Your password has been encoded and stored!")
+
+
+while True:
+    pass
+    break
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+
+
