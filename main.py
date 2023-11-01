709f8cd07e2cdc92b45ac8627a24a8b18995a3e0
CoryTheKing <cw2021fdbc@gmail.com>
2023-10-31 21:47:54 -0400

    made a few changes

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ def encode(password):       #new password to be encoded
             raise ValueError("Wrong key.")
     return ''.join(encoded_password)        #joins password
 
+def decode(password):       #new password to be encoded
+    encoded_password = []   # creates list with new encoded password
+    for digit in password:
+        if digit.isdigit():
+            encoded_digit = (int(digit) - 3) % 10
+            encoded_password.append(str(encoded_digit))
+        else:
+            raise ValueError("Wrong key.")
+    return ''.join(encoded_password)
+
 def menu():    #Menu goes into  main
     print("Menu\n" + "-------------\n" +
           "1. Encode\n" +
@@ -23,13 +33,12 @@ def main():         #puts everything into main
         menu()
         menu_choice = int(input("Please enter an option: "))
         if menu_choice == 1:
-            global password
             password = (input("Please enter your password to encode: "))
-            global encoded_password
             encoded_password = encode(password)
+            global encoded_password
             print("Your password has been encoded and stored!")  # print above encoded password
         elif menu_choice == 2:
-            print(f"The encoded password is {encoded_password}, and the original password is {password}")   #One whole function for bove
+            print(f"The encoded password is {encoded_password}, and the original password is {decode(password)}")   #One whole function for bove
         else:
             exit(0)
 
